1afb1d302eb924f3887dd8c87d2e227b15c63447
azozulya <bbbom@mail.ru>
2022-04-26 23:12:47 +0300

    fix: optimized vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -31,6 +31,17 @@ class VigenereCipheringMachine {
     if(!(message && key))
       throw new Error('Incorrect arguments!');
 
+    return this.mainTask('encrypt', message, key);
+  }
+
+  decrypt(encryptedMessage, key) {
+    if(!(encryptedMessage && key))
+      throw new Error('Incorrect arguments!');
+
+    return this.mainTask('decrypt', encryptedMessage, key);    
+  }
+
+  mainTask(type, message, key) {
     let result = '';
     let j = 0;
 
@@ -39,37 +50,6 @@ class VigenereCipheringMachine {
 
     for(let i = 0; i < message.length; i++) {
       const letter = message[i];
-      const letterCode = letter.charCodeAt();
-
-      if(letterCode >= 65 && letterCode <= 90) { // except space and other non-alphabetic characters
-        if(!key[j]) 
-          j = 0;
-
-        const keyCode = key[j].charCodeAt();
-        j++;
-  
-        result += String.fromCharCode((letterCode + keyCode) % 26 + 65); // because the Vigenere cipher starts with 0
-
-      } else {
-        result += letter;
-      }
-    }
-
-    return !this.isReverse && this.direct ? result.split('').reverse().join('') : result;    
-  }
-
-  decrypt(encryptedMessage, key) {
-    if(!(encryptedMessage && key))
-      throw new Error('Incorrect arguments!');
-
-    let result = '';
-    let j = 0;
-
-    encryptedMessage = encryptedMessage.toUpperCase();
-    key = key.toUpperCase();
-
-    for(let i = 0; i < encryptedMessage.length; i++) {
-      const letter = encryptedMessage[i];
 
       const letterCode = letter.charCodeAt();
 
@@ -79,18 +59,18 @@ class VigenereCipheringMachine {
 
         const keyCode = key[j].charCodeAt();
         j++;        
-        const code = (letterCode - keyCode + 26) % 26 + 65; //he offset of A is 0, the offset of B is 1 etc.
 
-        result += String.fromCharCode(code); // because the Vigenere cipher starts with 0
-
+        // +26, the offset of A is 0, the offset of B is 1 etc.
+        const code = type === 'decrypt' ? (letterCode - keyCode + 26) : (letterCode + keyCode);  
+        
+        result += String.fromCharCode(code % 26 + 65); // because the Vigenere cipher starts with 0
+        
       } else {
         result += letter;
       }
     }
-    return !this.isReverse && this.direct ? result.split('').reverse().join('') : result;  
-  }
-
-  
+    return !this.isReverse && this.direct ? result.split('').reverse().join('') : result;
+  }  
 }
 
 module.exports = {
